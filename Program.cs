5566671465f26c2b01426dea992c9c6f978bcacd
JKL <jarmogbg1975@outlook.com>
2024-02-27 15:13:12 +0100

    #First check in.

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using System;
+
+static void Main(string[] args) 
+{
+
+    Console.WriteLine("Press 1 to add new employee");
+    Console.WriteLine("Press 2 to show all employees");
+
+    var input = Console.ReadLine();
+
+    
+
+}
